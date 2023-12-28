6a3b0a3d49686f6b05c74b3944ae75da9bfc71b0
MorlyMathew <morlymathew1998@gmail.com>
2023-12-28 06:54:00 -0800

    redux

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import { Provider } from 'react-redux';
+import {name as appName} from './app.json';
+import MainStackNavigator from './src/navigation/AppNavigator';
+import store from './src/redux/Store';
+
+const RNRedux = ()=>(
+    <Provider store={store}>
+        <MainStackNavigator/>
+    </Provider>
+)
+AppRegistry.registerComponent(appName, () => RNRedux);
